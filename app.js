340c1e1ff052f386fefcc0589afdf49dbd09b96f
Irma Meza <imeza2@ivytech.edu>
2025-05-01 23:28:34 -0400

    M6 updates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 // tells app variable to be an express server 
 const router = express.Router()
 const bodyParser = require("body-parser")
-const Song = require("./models/songs")
+const Song = require("./models/song")
 const app = express()
 app.use(cors())
 
@@ -23,6 +23,17 @@ router.get("/songs", async (req, res) => {
     }
 })
 
+// grab single song in db 
+router.get("/songs/:id", async (req, res) => {
+    try {
+        const song = await Song.findById(req.params.id)
+        res.json(song)
+    }
+    catch(err) {
+        res.status(400).send(err)
+    }
+})
+
 //adding songs to db
 router.post("/songs", async (req, res) => {
     try {
@@ -36,35 +47,23 @@ router.post("/songs", async (req, res) => {
     }
 })
 
-//start web server, format -> app.listen(portnumber,function)
+// update is to update existing record/resource/database entry.. uses a put request
+router.put("/songs/:id", async (req, res) => {
+    //first need to find & update the song front end wants us to updates
+    // to do this, need to request id of song from req, find in db, update it
+    try {
+        const song = req.body
+        await Song.updateOne({ _id: req.params.id }, song)
+        console.log(song)
+        res.sendStatus(204)
+    }
+    catch (err) {
+        if (err) {
+            res.status(400).send(err)
+        }
+    }
+})
 
-//making api using routes
-// routes are used to handle browser requests, appear like URLs.
 
-// GET or regular request when someone goes to http://localhost:3000/hello
-// when using function in route, almost always have parameter or handle a response or request
-
-// router.get("/songs", function (req, res) {
-//     const songs = [
-//         {
-//             title: "We Found Love",
-//             artist: "Rihanna",
-//             popularity: 10,
-//             releaseDate: new Date(2011, 9, 22),
-//             genre: ["electro house"]
-//         },
-//         {
-//             title: "Happy",
-//             artist: "Pharrell Williams",
-//             popularity: 10,
-//             releaseDate: new Date(2013, 11, 21),
-//             genre: ["soul", "new soul"]
-//         }
-//      ];
-
-//     res.json(songs)
-// })
-
-// all requests that usually use an api start w /api.. so url would be localhost:3000/api/songs
 app.use("/api", router)
 app.listen(3000)
